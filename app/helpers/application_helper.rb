c57e98bee41558861d0092dbfed146c86f8d43f8
Joshua Lugo <atwfaj@gmail.com>
2016-05-14 15:06:00 -0500

    Revert "SC"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def current_user
-    @current_user
-  end
-
-  def logged_in?
-    !!current_user
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514155803) do
+ActiveRecord::Schema.define(version: 20160512232006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,13 +61,8 @@ ActiveRecord::Schema.define(version: 20160514155803) do
     t.text     "description"
     t.string   "zipcode"
     t.integer  "soundcloud_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "soundcloud_user_id"
-    t.string   "soundcloud_username"
-    t.string   "soundcloud_access_token"
-    t.string   "soundcloud_refresh_token"
-    t.datetime "soundcloud_expires_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
