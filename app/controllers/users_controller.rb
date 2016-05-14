020b248b43b09b416474cabd0dbe49f84244ad43
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-05-14 11:01:57 -0500

    javascript ready to deploy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,6 @@ class UsersController < ApplicationController
 	end
 
 	def callback
-		
+		p params
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'users#index'
 
-  post '/callback' => "users#callback"
+  get '/callback' => "users#callback"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
