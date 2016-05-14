1e21fa2b15df0c2bf12a0caa5a20e2b65af7e7f9
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-05-14 12:14:17 -0500

    stashing some changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,27 @@ class UsersController < ApplicationController
 		# create client object with app credentials
 		client = Soundcloud.new(:client_id => '50e414afb9a9f00311445bd1b9990164',
 		                        :client_secret => 'b599269690a3b1ce433c9fa3de01be74',
-		                        :redirect_uri => 'https://floating-tor-67033.herokuapp.com/callback')
+		                        :redirect_uri => 'http://localhost:3000/callback')
 
 		# exchange authorization code for access token
 		code = params[:code]
-		p code
-		access_token = client.exchange_token(:code => code)
-		p access_token
+		if params[:error]
+			redirect_to "/"
+		else
+			access_token = client.exchange_token(:code => code)
+			p access_token.access_token
+			sc_user = Soundcloud.new(:access_token => access_token.access_token)
+			sc_user = sc_user.get('/me')
+			user = User.new(soundcloud_id: sc_user.id)
+			p user
+			# if user.save
+				# session[:user_id] = User.id
+				p "Username:" + sc_user.username
+				p "Avatar Url: " + sc_user.avatar_url
+				p "Url: " + sc_user.permalink_url
+			# end
+		end
+
 	end
 end
+d
