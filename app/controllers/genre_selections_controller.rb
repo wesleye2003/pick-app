f23202d7c57395c5f7df1f55b1b8e88b7b270c39
Dave Kaiser <email": null>
2016-05-15 17:47:42 -0500

    add genre selections

--- app/controllers/genre_selections_controller.rb
+++ app/controllers/genre_selections_controller.rb
@@ -1,2 +1,14 @@
 class GenreSelectionsController < ApplicationController
+
+  def create
+    genre = Genre.find(params[:genre_id])
+    current_user.genres << genre
+  end
+
+  def destroy
+    genre = Genre.find(params[:id])
+    artist_genre = current_user.artist_genres.find_by(:genre_id)
+    artist_genre.destroy
+  end
+
 end
