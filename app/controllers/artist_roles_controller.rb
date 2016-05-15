8809b1f1abcfaf2d65f9a73dd63c91aed69be179
Dave Kaiser <email": null>
2016-05-15 15:05:55 -0500

    add artist-role methods

--- app/controllers/artist_roles_controller.rb
+++ app/controllers/artist_roles_controller.rb
@@ -1,3 +1,14 @@
 class ArtistRolesController < ApplicationController
 
+  def create
+    role = Role.find(params[:role_id])
+    current_user.roles << role
+  end
+
+  def destroy
+    role = Role.find(params[:id])
+    artist_role = current_user.artist_roles.find_by(:role_id)
+    artist_role.destroy
+  end
+
 end
