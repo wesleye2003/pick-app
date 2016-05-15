a88bba3ddd6fcc84748fe36cfaa2abe6ba2c4f34
Dave Kaiser <email": null>
2016-05-15 15:18:31 -0500

    add s-roles methods

--- app/controllers/searched_roles_controller.rb
+++ app/controllers/searched_roles_controller.rb
@@ -1,2 +1,12 @@
 class SearchedRolesController < ApplicationController
+  def create
+    role = Role.find(params[:role_id])
+    current_user.s_roles << role
+  end
+
+  def destroy
+    role = Role.find(params[:id])
+    s_role = current_user.s_roles.find_by(:role_id)
+    s_role.destroy
+  end
 end
