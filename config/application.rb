5431c5519425d886db027b0a99f131cc81e6793d
Erica Lloyd <email": null>
2016-05-16 16:08:06 -0500

    Update cors config in application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,13 +23,6 @@ module Pick
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-     # Configure Rack CORS
-    # config.middleware.use Rack::Cors do
-    #   allow do
-    #     origins '*'
-    #     resource '*', :headers => :any, :methods => [:get, :post, :put, :options]
-    #   end
-    # end
 
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
