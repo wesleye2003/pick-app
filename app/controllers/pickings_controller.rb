e926bc83dbac9dc9272870ae71dfcacc22f5d1ec
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-05-16 09:25:10 -0500

    change sessions, routes, and user

--- app/controllers/pickings_controller.rb
+++ app/controllers/pickings_controller.rb
@@ -1,2 +1,11 @@
 class PickingsController < ApplicationController
+	def index
+		user = User.find(params[:id])
+		if user
+			picks = user.pickings
+			render json: picks
+		else
+			400
+		end
+	end
 end
