93cc9bd7ff73fc9612a466f71eafc19a7e6fc144
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-05-16 22:18:15 -0500

    add routes for edit roles

--- app/controllers/artist_roles_controller.rb
+++ app/controllers/artist_roles_controller.rb
@@ -1,13 +1,15 @@
 class ArtistRolesController < ApplicationController
 
   def create
+  	artist = User.find_by(params[:id])
     artist_role = ArtistRole.find_or_create_by(role_id: params[:id], user_id: params[:user_id])
   end
 
   def destroy
-    artist_role = ArtistRole.find_by(role_id: params[:id], user_id: params[:user_id])
-    if artist_role
-    	artist_role.destroy
+    artist = User.find_by(params[:id])
+    artist_roles = artist.artist_roles
+    if artist_roles
+    	artist_roles.destroy_all
     end
   end
 
