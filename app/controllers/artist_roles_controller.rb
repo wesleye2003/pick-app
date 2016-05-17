4357f0dae648605adaec64061c72a25e32467be3
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-05-16 22:23:50 -0500

    fix server error

--- app/controllers/artist_roles_controller.rb
+++ app/controllers/artist_roles_controller.rb
@@ -1,12 +1,12 @@
 class ArtistRolesController < ApplicationController
 
   def create
-  	artist = User.find_by(params[:id])
+  	artist = User.find(params[:id])
     artist_role = ArtistRole.find_or_create_by(role_id: params[:id], user_id: params[:user_id])
   end
 
   def destroy
-    artist = User.find_by(params[:id])
+    artist = User.find(params[:id])
     artist_roles = artist.artist_roles
     if artist_roles
     	artist_roles.destroy_all
