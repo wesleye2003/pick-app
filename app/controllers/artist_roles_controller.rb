21174ff201460a39d87a4fa6bb207edfa7cb278c
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-05-16 22:33:52 -0500

    refine role edits

--- app/controllers/artist_roles_controller.rb
+++ app/controllers/artist_roles_controller.rb
@@ -3,6 +3,8 @@ class ArtistRolesController < ApplicationController
   def create
   	artist = User.find(params[:id])
     artist_role = ArtistRole.find_or_create_by(role_id: params[:id], user_id: params[:user_id])
+    message = {'status' => 'Changes Saved Successfully.'}
+		render :json => message
   end
 
   def destroy
@@ -10,7 +12,12 @@ class ArtistRolesController < ApplicationController
     artist_roles = artist.artist_roles
     if artist_roles
     	artist_roles.destroy_all
+    	message = {'status' => 'Roles erased successfully.'}
+		else
+			400
+			message = {'status' => 'Roles not erased!'}
     end
+    render :json => message
   end
 
 end
