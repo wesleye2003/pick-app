188f0b0c8fb3ad0349e57c46cf79019eaa066f11
davidwkaiser <davidwkaiser@yahoo.com>
2016-05-18 17:46:13 -0500

    add a test of searched role creation

--- app/controllers/searched_roles_controller.rb
+++ app/controllers/searched_roles_controller.rb
@@ -1,6 +1,5 @@
 class SearchedRolesController < ApplicationController
   def create
-  	artist = User.find(params[:id])
     artist_role = SearchedRole.find_or_create_by(role_id: params[:id], user_id: params[:user_id])
     message = {'status' => 'Changes Saved Successfully.'}
 		render :json => message
