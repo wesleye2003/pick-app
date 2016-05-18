22189293c23dd8d0b383909b98595d959b0df2cd
davidwkaiser <davidwkaiser@yahoo.com>
2016-05-18 13:49:12 -0500

    progress on tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,22 +3,20 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   test "zip code is required" do
-    @mick = User.new(username: "Mick Jagger", password: "password")
-     assert_not @mick.save
+     assert_not users(:mick).save
   end
 
-  test "password is required" do
-    @keith = User.new(username: "Keith Richards", zipcode: "60606")
-     assert_not @keith.save
-  end
+  # test "password is required" do
+  #   @keith = User.new(username: "Keith Richards", zipcode: "60606")
+  #    assert_not @keith.save
+  # end
 
   test "confirms class" do
         assert User.new.class == User
   end
 
   test "takes three parameters and returns a User object" do
-    @jimi = User.new(username: "Jimi Hendrix", zipcode: "60622", password: "password")
-        assert @jimi.instance_of?(User) == true
+        assert_instance_of(User, users(:jimi))
   end
 
 
