22347b2023282814ba1049798d5d022535b7861f
davidwkaiser <davidwkaiser@yahoo.com>
2016-05-18 09:39:49 -0500

    add five tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user_count" do
+    assert_equal 4, User.count
+  end
+
+  test "zip code is required" do
+    mick = User.new(username: "Mick Jagger", password: "password")
+     assert_not mick.save
+  end
+
+  test "password is required" do
+    keith = User.new(username: "Keith Richards", zipcode: "60606")
+     assert_not keith.save
+  end
+
+  test "confirms class" do
+        assert User.new.class == User
+  end
+
+  test "takes three parameters and returns a User object" do
+    jimi = User.new(username: "Jimi Hendrix", zipcode: "60622", password: "password")
+        assert jimi.instance_of?(User) == true
+  end
+
+
+
 end
