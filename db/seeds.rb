8fa3f04286dff626b0c5a0aff1c089dfba20b565
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-05-19 14:16:12 -0500

    done with seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 User.destroy_all
 Genre.destroy_all
 GenreSelection.destroy_all
@@ -20,36 +21,36 @@ funk = Genre.create(name: "Funk")
 jazz = Genre.create(name: "Jazz")
 classic = Genre.create(name: "Classical")
 alt = Genre.create(name: "Alternative")
-
+rb = Genre.create(name: "R&B")
 
 #Roles
 drum = Role.create(name: "Drummer")
 vocal = Role.create(name: "Vocalist")
 guitar = Role.create(name: "Guitarist")
 bass = Role.create(name: "Bassist")
-piano = Role.create(name: "Pianist/Keys")
+piano = Role.create(name: "Pianist")
 
 
 wes = User.new(username: "Wesley El-Amin",
-		   description: "I bring the bass every place I go.",
-		   zipcode: '60610',
+		       description: "I bring the bass every place I go.",
+		       zipcode: '60610',
                avatar_url: 'http://i.imgur.com/yPyPWD4.png')
 wes.password = "password"
 wes.city = 'Chicago'
 wes.save
 
 josh = User.new(username: "joshulu",
-		     description: "Piano is the (pentatonic) key to my heart.",
-	           zipcode: '60506',
-                 avatar_url: 'http://i.imgur.com/yPyPWD4.png')
+		        description: "Piano is the (pentatonic) key to my heart.",
+	            zipcode: '60506',
+                avatar_url: 'http://i.imgur.com/yPyPWD4.png')
 josh.password = "password"
 josh.city = 'Chicago'
 josh.save
 
 david = User.new(username: "David Kaiser",
-		     description: "Rock Dove by day, Rock Demigod by night.",
-		     zipcode: '60201',
-                 avatar_url: 'http://i.imgur.com/yPyPWD4.png')
+		        description: "Rock Dove by day, Rocker by night.",
+		        zipcode: '60201',
+                avatar_url: 'http://i.imgur.com/yPyPWD4.png')
 david.password = "password"
 david.city = 'Chicago'
 david.save
@@ -74,7 +75,8 @@ dave.save
 stevie = User.new(username: "Stevie Ray",
                    description: "I play guitar and sing.",
                    zipcode: '78704', 
-                   avatar_url: 'http://cps-static.rovicorp.com/3/JPG_400/MI0001/399/MI0001399374.jpg?partner=allrovi.com')
+                   avatar_url: 'http://cps-static.rovicorp.com/3/JPG_400/MI0001/399/MI0001399374.jpg?partner=allrovi.com',
+                   permalink: "https://soundcloud.com/stevie-ray-vaughan-official")
 stevie.password = "password"
 stevie.city = 'Austin'
 stevie.save
@@ -82,23 +84,26 @@ stevie.save
 james = User.new(username: "James Brown",
             description: "THE king of soul.",
             zipcode: '60606',
-            avatar_url: 'http://a2.files.biography.com/image/upload/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTIwNjA4NjMzNzU0ODQ2NzMy.jpg')
+            avatar_url: 'http://a2.files.biography.com/image/upload/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTIwNjA4NjMzNzU0ODQ2NzMy.jpg',
+            permalink: 'https://soundcloud.com/jamesbrownofficial')
 james.password = "password"
 james.city = 'Chicago'
 james.save
 
 drake = User.new(username: "Drake",
-            description: "You used to call me on my cell phone...",
+            description: "You used to call me on my cell phone :(",
             zipcode: '60606',
-            avatar_url: 'http://assets.rollingstone.com/assets/images/video/drake-takes-on-lil-wayne-jay-z-and-a-rod-on-snl-20140119/20140118-drake-thumb-306x306-1390145726.jpg')
+            avatar_url: 'http://assets.rollingstone.com/assets/images/video/drake-takes-on-lil-wayne-jay-z-and-a-rod-on-snl-20140119/20140118-drake-thumb-306x306-1390145726.jpg',
+            permalink: 'https://soundcloud.com/beats1ovosoundradio')
 drake.password = "password"
 drake.city = 'Chicago'
 drake.save
 
 frank = User.new(username: "Frank Zappa",
-            description: "Self-explanatory",
+            description: "Self-explanatory...",
             zipcode: '60606',
-            avatar_url: 'http://www.allaboutjazz.com/media/large/8/0/a/54d540396974b94487453454281ea.jpg')
+            avatar_url: 'http://www.allaboutjazz.com/media/large/8/0/a/54d540396974b94487453454281ea.jpg',
+            permalink: 'https://soundcloud.com/frankzappa')
 frank.password = "password"
 frank.city = 'Chicago'
 frank.save
@@ -106,32 +111,46 @@ frank.save
 ray = User.new(username: "Ray Charles",
             description: "Georgia on my mind",
             zipcode: '60606',
-            avatar_url: 'http://spectrummagazine.org/sites/default/files/imagecache/large_article_image/files/RayCharles.jpg')
+            avatar_url: 'http://spectrummagazine.org/sites/default/files/imagecache/large_article_image/files/RayCharles.jpg',
+            permalink: 'https://soundcloud.com/raycharles')
 ray.password = "password"
 ray.city = 'Chicago'
 ray.save
 
+bey = User.new(username: "Beyoncé",
+            description: "Queen of the Beyhive.",
+            zipcode: '60606',
+            avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhqyMvL8696Yt5XIlTp_bmRUim2TbUDSOClW6m-WFqI5UF6x_U',
+            permalink: 'https://soundcloud.com/beyonce')
+bey.password = "password"
+bey.city = 'Chicago'
+bey.save
+
+
 # Add Roles
-piano.users << [josh, mason, andre, jack, alex, dom, ray]
-drum.users << [ryan, andre, lisa, sami, tom]
-vocal.users << [david, tom, stevie, james, drake, dave, frank, ray]
-guitar.users << [david, ryan, joe, jim, stevie, dave, frank]
-bass.users << [wes, ryan, eric, sami]
+piano.users << [ray, josh, erica, ray, bey]
+drum.users << [josh, erica, david]
+vocal.users << [drake, ray, frank, james, bey]
+guitar.users << [dave, david, frank]
+bass.users << [wes, drake]
+
 
 # Add Genres
-rock.users << [david, lisa, joe, sami, dave, frank]
-hip_hop.users << [wes, ryan, andre, tom, drake]
-jazz.users << [wes, mason, jack, dom, jim, stevie, ray]
-funk.users << [david, ryan, lisa, eric, jim, james]
-classic.users << [josh, jack, alex, sami, drake]
-alt.users << [dom, joe, eric, tom, dave, frank]
+rock.users << [josh, frank, dave, david]
+hip_hop.users << [wes, drake, josh]
+jazz.users << [wes, ray, james, erica]
+funk.users << [james, ray, david]
+classic.users << [frank, james, stevie, wes]
+alt.users << [dave, erica]
+rb.users << [bey, drake, james]
 
 #Add Requests
 drake.request(wes)
 david.request(wes)
 dave.request(wes)
 josh.request(wes)
-mason.request(wes)
+erica.request(wes)
+bey.request(wes)
 
 
 
